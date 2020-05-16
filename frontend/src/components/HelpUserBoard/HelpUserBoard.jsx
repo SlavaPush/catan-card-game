eded64d211fe8cc473752446bb31e650857b125e
Artem <ArtShilov1991@yandex.ru>
2020-05-16 18:46:20 +0300

    hover template

--- frontend/src/components/HelpUserBoard/HelpUserBoard.jsx
+++ frontend/src/components/HelpUserBoard/HelpUserBoard.jsx
@@ -1,40 +1,55 @@
-import React from 'react'
-import styled from 'styled-components'
-import { useSelector } from 'react-redux'
+import React, { useState } from "react";
+import styled, {css} from "styled-components";
+// import { useSelector } from "react-redux";
+import LogicPart2 from './components/LogicPart2';
 
 export default function HelpUserBoard() {
-    const HelperDiv = styled.div`
-        margin: auto;
-        background-color: green;
-        padding: 15px 40px;
-        width: 150px;
-        border-radius: 4px;
-        color: white;
-        text-align: center;
-        vertical-align: middle;
-        font-size: 14px;
-    `
-    const Prompt = styled.h3`
+  const [close,setClose] = useState(true)
+
+  // const HelperDiv = styled.div`
+  //   margin: auto;
+  //   background-color: green;
+  //   padding: 15px 40px;
+  //   width: 150px;
+  //   border-radius: 4px;
+  //   color: white;
+  //   text-align: center;
+  //   vertical-align: middle;
+  //   font-size: 14px;
+  // `;
+  const Prompt = styled.h3`
     color: white;
-    margin-bottom:7px;
-    margin: 0px
-`
-const Stage = styled.p`
-margin: 5px
-`
-    const helpRule = useSelector(state => state.template.prompt)
-    const step = useSelector(state => state.steps.step)
+    margin-bottom: 7px;
+    margin: 0px;
+    ${(close)=> close && 
+      css`color: #FFFAFA ;
+      margin: 0 ;
+      padding: 10px 15px 10px 7px;
+      width: 300px;
+      border-radius: 4px;
+      text-align: center;
+      vertical-align: middle;
+      font-size: 20px;
+      background-color: green;
+      &:hover {
+        background-color: palevioletred;
+        color: white;
+      }
+    ` }
+  `;
+  // const Stage = styled.p`
+  //   margin: 5px;
+  // `;
 
-    return (
-      <>
-        
-          {
-          step[0]? <HelperDiv><Prompt>Подсказка:<Stage>Этап: {helpRule[0].stepName}</Stage></Prompt>{helpRule[0].rule}</HelperDiv>:
-          step[1]? <HelperDiv><Prompt>Подсказка:<Stage>Этап: {helpRule[1].stepName}</Stage></Prompt>{helpRule[1].rule}</HelperDiv>:
-          step[2]? <HelperDiv><Prompt>Подсказка:<Stage>Этап: {helpRule[2].stepName}</Stage></Prompt>{helpRule[2].rule}</HelperDiv>:
-          <HelperDiv><Prompt>Подсказка:</Prompt>Ход соперника</HelperDiv>
-          }
 
-        </>
-    )
+
+  return (
+    <>
+    {
+      
+      close?<Prompt onClick={()=>setClose(false)}>Подсказка</Prompt>:   <LogicPart2 close={close} ActionClick={()=>setClose(true)}  />
+
+    }
+    </>
+  );
 }
