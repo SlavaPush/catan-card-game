b1ad5f12aa8d24b61f5a1fb081b3db1256e16ec7
Artem <ArtShilov1991@yandex.ru>
2020-05-22 12:26:27 +0300

    sfsdf

--- frontend/src/sockets/socket.js
+++ frontend/src/sockets/socket.js
@@ -11,7 +11,7 @@ import {messageReceived} from '../Redux/chat-actions';
 
 const setupSocket = (dispatch, callBack) => {
     const socket = new WebSocket(window.location.origin.replace(/^http/, 'ws')); // DEPLOY
-
+    // const socket = new WebSocket('ws://localhost:3001'); 
     socket.onopen = callBack
     
     socket.onmessage = (event) => {
