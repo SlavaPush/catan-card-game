f336232df6e6cf5b527f1e974cfe4f392fe6bd22
Vyacheslav Pyshkin <slavapyshkin1@gmail.com>
2020-05-22 09:37:54 +0300

    friday

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useCallback } from 'react';
+import { useParams  } from "react-router-dom";
 import DevelopCardsRow from './components/DevelopCardsRow';
 import PlayerCardsRow from './components/PlayerCardsRow';
 import MarketCardsRow from './components/MarketCardsRow';
@@ -16,28 +17,29 @@ import {
   ContainerControlPanel
   } from './components/CommonStyledComponents/ScApp'
 
-function App({ match }) {
-  const modalNameCard = useSelector(state => state.cards.modalNameCard)
+function App() {
+  const modalNameCard = useSelector(state => state.cards.modalNameCard);
+  const {player, id} = useParams();
 
   const dispatch = useDispatch()
-  if (match.params.player) {
-    localStorage.setItem('player', match.params.player);
+  if (player) {
+    localStorage.setItem('player', player);
   }
-  const urlPl2 = `http://localhost:3000/${match.params.id}/player2`
+  const urlPl2 = `http://localhost:3000/${id}/player2`
 
 
   const state = useSelector(state => state.cards);
 
   useEffect(() => {
-    if (match.params.player === 'player1') {
-      dispatch(sagaStateTransfer(match.params.id, state))
+    if (player === 'player1') {
+      dispatch(sagaStateTransfer(id, state))
     }
 
   }, [dispatch]);
 
   useEffect(() => {
-    if (match.params.player === 'player2') {
-      dispatch(sagaSearchStateInRoom(match.params.id));
+    if (player === 'player2') {
+      dispatch(sagaSearchStateInRoom(id));
     }
   }, [dispatch]);
 
